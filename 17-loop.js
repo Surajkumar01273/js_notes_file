b823ae52531ddad49bdb818459e2eb37f98f9200
suraj <surajshk1995@gmail.com>
2024-08-23 22:13:32 +0530

    add loop file

---
+++ 17-loop.js
@@ -0,0 +1,24 @@
+// for
+
+for (let i = 0; i <=10; i++) {
+    const element = i;
+    if(element===5){
+        // console.log(`the best number ${element}`);     
+    }
+    // console.log(element);   
+}
+
+for (let i = 1; i <=5; i++) {
+    const element = i;
+    // console.log(`Table Of ${element}`);
+    console.log("*");
+
+    for (let j = i; j <=i; j++) {
+        const element = j;
+        // console.log(`${i} * ${j} = ${i*j}`);
+        console.log("*");
+    }
+    
+    
+}
+
