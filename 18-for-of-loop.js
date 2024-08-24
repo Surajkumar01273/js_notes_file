44206168bbba6e9bc92b71e137e5bff8c766bd32
suraj <surajshk1995@gmail.com>
2024-08-24 18:45:46 +0530

    update file some code

--- 18-for-of-loop.js
+++ 18-for-of-loop.js
@@ -28,11 +28,16 @@ map.set('UK', 'United State Kingdom')
 //     console.log(key, ':-', value);
 // }
 
-const myObject = {
-    'game1': 'NFS',
-    "game2": 'spiderman'
-}
+// const myObject = {
+//     'game1': 'NFS',
+//     "game2": 'spiderman'
+// }
 
-for (const [obj, value] of myObject) {
-    console.log(obj, ':-', value);
-}
+// for (const [obj, value] of myObject) {   //   for of loop not iterate object mens not work on object
+//     console.log(obj, ':-', value);
+// }
+
+// for (const key in map) {    // for in loop not iterate map mens not used for in loop on map function
+//     console.log(key);
+   
+// }
